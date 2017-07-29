76bde92e6d94bd831ed6926074482b4dfe68d40d
Tom Revill <tom.revill@gmail.com>
2017-07-29 17:02:30 +1200

    draft home page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import Home from './Home'
+import Task from './Task'
+
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div className="container">
+      <h1>Task Mannager!</h1>
+      <Router>
+        <div>
+          <Route exact path='/' component={Home}/>
+          <Route path='/Task' component={Task}/>
+        </div>
+      </Router>
+    </div>
   )
 }
 
 export default App
-
---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function (props){
+  return(
+    <div className='Home'>
+        <h2>Please select a task or create a task!</h2>
+        <div className='Task'>
+          Create a task by clicking here. 
+        </div>
+    </div>
+  )
+}
